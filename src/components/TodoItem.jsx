ec4c9924dd1a13e708719e1263cff299c9098861
taejin7088 <gog70888@gmail.com>
2025-02-03 02:37:44 +0900

    item 스타일틀끝

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,11 +4,32 @@ import styled from "styled-components";
 const TodoItemStyle = styled.div`
   .item {
     width: 200px;
-    height: 200px;
-    border: 1px solid black;
+    height: 300px;
+    border: 3px solid green;
+    border-radius: 10px;
     margin: 20px;
     display: flex;
     flex-direction: column;
+  }
+  .item-title {
+    margin-top: 10px;
+    margin-left: 10px;
+    font-size: 30px;
+  }
+  .item-content {
+    margin: 10px 10px 0px 10px;
+  }
+  .item-buttons {
+    display: flex;
+    justify-content: space-evenly;
+    margin-top: auto;
+    margin-bottom: 10px;
+  }
+  .item-buttons button {
+    height: 30px;
+    width: 80px;
+    border: solid 1px;
+    border-radius: 5px;
   }
 `;
 
@@ -19,6 +40,10 @@ const TodoItem = ({ todo }) => {
         <div className="item">
           <div className="item-title">{todo.title}</div>
           <div className="item-content">{todo.content}</div>
+          <div className="item-buttons">
+            <button style={{ backgroundColor: "red" }}>dd</button>
+            <button>dd</button>
+          </div>
         </div>
       </TodoItemStyle>
     </>
